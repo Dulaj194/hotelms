4f474358e85991fcdaa6cec2e8a2b8d4cd02e061
Dulaj Prabhashana <dulajprabhashana45@gmail.com>
2026-04-25 11:24:17 +0530

    test1

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
+
 from app.api.router import router
 from app.core.config import settings
 from app.core.logging import configure_logging, get_logger
