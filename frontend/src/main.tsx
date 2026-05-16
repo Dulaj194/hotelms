40d9e1223ed8688a25e5e67cabac89768a643945
Dulaj Prabhashana <dulajprabhashana45@gmail.com>
2026-05-17 00:11:13 +0530

    initialize frontend project with HTML entry point and React entry script

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/config";
+import i18n from "i18next";
+
+// Update HTML lang attribute on language change
+i18n.on('languageChanged', (lng) => {
+  document.documentElement.lang = lng;
+});
+
+// Initial set
+document.documentElement.lang = i18n.language;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
